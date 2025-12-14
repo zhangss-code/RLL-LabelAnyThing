e4be853f11461802d94e135322b6d89937c0ceac
zhangss-code <2026140349@qq.com>
2025-12-14 21:16:38 +0800

    first to commit

---
+++ src/A_CAM_P.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+def A_CAM_P():
+    
+    Open_camrea = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    if not Open_camrea.isOpened():
+        print("摄像头可能出现问题，请检查摄像头连接。")
+
+    jpg = Open_camrea.read()[1]
+    if jpg is None:
+        print("摄像头读取失败，请检查摄像头连接。")
+        return None
+    
+    print("摄像头读取成功。")
+    # 显示图像
+    cv2.imshow("Camera", jpg)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    Open_camrea.release()
+
+    
+A_CAM_P()
